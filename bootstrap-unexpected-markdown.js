3e8d6ba22af11321447d9111a012364e8515a72f
Andreas Lind <andreaslindpetersen@gmail.com>
2019-01-19 22:50:57 +0100

    Work around unexpected-markdown bug

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,5 +1,7 @@
 /*global unexpected:true*/
 unexpected = require('unexpected');
-unexpected = unexpected.clone();
+// There's a bug in unexpected-markdown that causes npm test
+// not to work unless this is done on the global instance:
 unexpected.output.preferredWidth = 80;
+unexpected = unexpected.clone();
 unexpected.use(require('./lib/unexpectedImage'));
